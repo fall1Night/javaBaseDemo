5baa3c969e11c0aae6ebc7df641fa3c901f07370
shixc <1375767865@qq.com>
2022-07-18 17:15:07 +0800

    添加锁相关内容

--- src/main/java/com/example/learningdemo/Thread_12/ThreadTest.java
+++ src/main/java/com/example/learningdemo/Thread_12/ThreadTest.java
@@ -44,6 +44,22 @@ import java.util.concurrent.*;
     yield()
     对静态方法 Thread.yield() 的调用声明了当前线程已经完成了生命周期中最重要的部分，可以切换给其它线程来执行。
     该方法只是对线程调度器的一个建议，而且也只是建议具有相同优先级的其它线程可以运行。
+四、中断
+    InterruptedException
+    通过调用一个线程的 interrupt() 来中断该线程，如果该线程处于阻塞、限期等待或者无限期等待状态，那么就会抛出 InterruptedException，从而提前结束该线程。
+    但是不能中断 I/O 阻塞和 synchronized 锁阻塞。
+    对于以下代码，在 main() 中启动一个线程之后再中断它，由于线程中调用了 Thread.sleep() 方法，
+    因此会抛出一个 InterruptedException，从而提前结束线程，不执行之后的语句。
+    interrupted()
+    如果一个线程的 run() 方法执行一个无限循环，并且没有执行 sleep() 等会抛出 InterruptedException 的操作，那么调用线程的 interrupt() 方法就无法使线程提前结束。
+    但是调用 interrupt() 方法会设置线程的中断标记，此时调用 interrupted() 方法会返回 true。
+    因此可以在循环体中使用 interrupted() 方法来判断线程是否处于中断状态，从而提前结束线程。
+    Executor 的中断操作
+    调用 Executor 的 shutdown() 方法会等待线程都执行完毕之后再关闭，但是如果调用的是 shutdownNow() 方法，则相当于调用每个线程的 interrupt() 方法。
+    以下使用 Lambda 创建线程，相当于创建了一个匿名内部线程。\
+五、互斥同步
+    同步锁synchronized
+
 * */
 public class ThreadTest {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
@@ -54,12 +70,11 @@ public class ThreadTest {
         threadRunnable.start();
 
         // 将线程开启守护线程
-        threadRunnable.setDaemon(true);
+//        threadRunnable.setDaemon(true);
         // 休眠当前线程300ms
         Thread.sleep(300);
         // 建议切换其他线程执行
         Thread.yield();
-
 
 
         //使用 Callable 实例创建一个 Thread 实例,与runnable区别为可以又返回值,返回值通过 FutureTask 进行封装。
@@ -105,9 +120,71 @@ public class ThreadTest {
         executorServiceByExecutors.shutdown();
 
 
+        System.out.println("测试InterruptedException()中断线程-------------------------------------------------------");
+        //通过调用一个线程的 interrupt() 来中断该线程，如果该线程处于阻塞、限期等待或者无限期等待状态，
+        // 那么就会抛出 InterruptedException，从而提前结束该线程。但是不能中断 I/O 阻塞和 synchronized 锁阻塞。
+        Thread threadByTestInterrupted = new MyThreadByTestInterrupted();
+        threadByTestInterrupted.start();
+        threadByTestInterrupted.interrupt();
+        System.out.println("threadByTestInterrupted  run----");
 
+
+        System.out.println("循环体中使用 interrupted() 方法来判断线程是否处于中断状态，从而提前结束线程-------------------------------------------------------");
+        // 如果一个线程的 run() 方法执行一个无限循环，并且没有执行 sleep() 等会抛出 InterruptedException 的操作，那么调用线程的 interrupt() 方法就无法使线程提前结束。
+        // 但是调用 interrupt() 方法会设置线程的中断标记，此时调用 interrupted() 方法会返回 true。
+        // 因此可以在循环体中使用 interrupted() 方法来判断线程是否处于中断状态，从而提前结束线程。
+        Thread myThreadInterrupted = new MyThreadInterrupted();
+        myThreadInterrupted.start();
+        myThreadInterrupted.interrupt();
+
+        System.out.println("Executor 的中断操作-------------------------------------------------------------------------");
+        //调用 Executor 的 shutdown() 方法会等待线程都执行完毕之后再关闭，但是如果调用的是 shutdownNow() 方法，则相当于调用每个线程的 interrupt() 方法。
+        ExecutorService executorThreadPool = Executors.newCachedThreadPool();
+        executorThreadPool.execute(() -> {
+            try {
+                Thread.sleep(2000);
+                System.out.println("Thread run");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        executorThreadPool.shutdownNow();
+        System.out.println("Main run");
+
+
+        // 测试synchronized同步锁线程
+        //它只作用于同一个对象，如果调用两个对象上的同步代码块，就不会进行同步。
+        //对于以下代码，使用 ExecutorService 执行了两个线程，由于调用的是同一个对象的同步代码块，因此这两个线程会进行同步，当一个线程进入同步语句块时，另一个线程就必须等待。
+        System.out.println("测试synchronized同步锁线程--同一个线程调用两次代码块-----------------------------------------------------------------------");
+/*        ThreadTest ThreadBySynchronized = new ThreadTest();
+        ExecutorService ExecutorBySynchronized = Executors.newCachedThreadPool();
+        ExecutorBySynchronized.execute(() -> ThreadBySynchronized.synchronizedTest());
+        ExecutorBySynchronized.execute(() -> ThreadBySynchronized.synchronizedTest());*/
+
+        // 两个线程调用了不同对象的同步代码块，因此这两个线程就不需要同步。从输出结果可以看出，两个线程交叉执行。
+        System.out.println("测试synchronized同步锁线程--两个线程调用同一个代码块-------------------------------------------------------------------------");
+        ThreadTest ThreadBySynchronized1 = new ThreadTest();
+        ThreadTest ThreadBySynchronized2 = new ThreadTest();
+        ExecutorService ExecutorBySynchronized1 = Executors.newCachedThreadPool();
+        ExecutorBySynchronized1.execute(() -> ThreadBySynchronized1.synchronizedTest());
+        ExecutorBySynchronized1.execute(() -> ThreadBySynchronized2.synchronizedTest());
     }
 
+
+    // 测试synchronized同步锁
+    public void synchronizedTest() {
+        synchronized (this) {
+            for (int i = 0; i < 10; i++) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public synchronized void CommonTest() {
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
 }
 
 //实现 Runnable 接口,需要实现接口中的 run() 方法。
@@ -131,3 +208,29 @@ class MyThread extends Thread {
         System.out.println("继承Thread跑起来了");
     }
 }
+
+// 测试InterruptedException()中断线程
+class MyThreadByTestInterrupted extends Thread {
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(2000);
+            System.out.println("Thread run");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+// interrupted() 判断当前线程是否处于中断状态,从而提前结束进程
+class MyThreadInterrupted extends Thread {
+    @Override
+    public void run() {
+        while (!interrupted()) {
+            // ..
+        }
+        System.out.println("MyThreadInterrupted 处于中断状态,结束进程");
+    }
+}
+
+
