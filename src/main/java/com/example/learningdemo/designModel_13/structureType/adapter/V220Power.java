221c79d3756417a288a48801934fc06f96dc4b10
shixc <1375767865@qq.com>
2022-09-27 10:25:45 +0800

    适配器模式

---
+++ src/main/java/com/example/learningdemo/designModel_13/structureType/adapter/V220Power.java
@@ -0,0 +1,13 @@
+package com.example.learningdemo.designModel_13.structureType.adapter;
+
+/**
+ * 我们拥有的家用电是220v
+ */
+
+public class V220Power {
+
+    public int provideV220Power() {
+        System.out.println("现有类: 我们提供的是220v的家用电");
+        return 220;
+    }
+}
---
+++ src/main/java/com/example/learningdemo/designModel_13/structureType/adapter/V5Power.java
@@ -0,0 +1,12 @@
+package com.example.learningdemo.designModel_13.structureType.adapter;
+
+
+/**
+ * 可以看出，手机依赖一个提供5V电压的接口：
+ * 提供5v电压的接口
+ */
+
+public interface V5Power {
+
+    public int provideV5Power();
+}
