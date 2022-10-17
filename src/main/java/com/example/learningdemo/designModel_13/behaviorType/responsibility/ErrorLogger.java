39aecd2f2234613c3c6bb4d9de94455553b4bb5d
shixc <1375767865@qq.com>
2022-10-17 16:49:15 +0800

    责任链模式

---
+++ src/main/java/com/example/learningdemo/designModel_13/behaviorType/responsibility/ErrorLogger.java
@@ -0,0 +1,13 @@
+package com.example.learningdemo.designModel_13.behaviorType.responsibility;
+
+public class ErrorLogger extends AbstractLogger {
+
+    public ErrorLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error Console::Logger: " + message);
+    }
+}
--- src/main/java/com/example/learningdemo/designModel_13/behaviorType/responsibility/responsibilityTest.java
+++ src/main/java/com/example/learningdemo/designModel_13/behaviorType/responsibility/responsibilityTest.java
@@ -1,5 +1,6 @@
 package com.example.learningdemo.designModel_13.behaviorType.responsibility;
 
+
 /**
  * @Author sxc
  * @Description 责任链模式
@@ -10,7 +11,31 @@ public class responsibilityTest {
      * 责任链模式（Chain of Responsibility Pattern）为请求创建了一个接收者对象的链。这种模式给予请求的类型，对请求的发送者和接收者进行解耦。
      * 这种类型的设计模式属于行为型模式。在这种模式中，通常每个接收者都包含对另一个接收者的引用。
      * 如果一个对象不能处理该请求，那么它会把相同的请求传给下一个接收者，依此类推。
-     *
-     *
+     * <p>
+     * 需求:打印日志,若不符合当前日志类型则不答应传递给下一个日志进行判断
      */
+    private static AbstractLogger getChainOfLoggers() {
+
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return errorLogger;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        AbstractLogger loggerChain = getChainOfLoggers();
+
+        loggerChain.logMessage(AbstractLogger.INFO, "This is an information.");
+
+        loggerChain.logMessage(AbstractLogger.DEBUG,
+                "This is a debug level information.");
+
+        loggerChain.logMessage(AbstractLogger.ERROR,
+                "This is an error information.");
+    }
 }
